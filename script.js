e087503a3b4c57f47cea2f3d1ae39643106157b7
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2024-01-17 16:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	get getName{
+		return this.name;
+	}
+	set setAge(age){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor{
+		super(name,age);
+	}
+	study(name){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor{
+		super(name,age);
+	}
+teach(name){
+	console.log(`${this.name} is teaching`);
+}
+}
 
 // Do not change the code below this line
 window.Person = Person;
